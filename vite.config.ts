835c656368a42ac6da115682b5d0d2ac81f59195
mandyjeng <earldom.jeng@gmail.com>
2025-12-26 10:11:46 +0800

    Configure base path for the project
    Set the base path for the project to '/hanoi/'.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    base: '/hanoi/', // 加上這行，hanoi 是你的專案名稱
+    plugins: [react()], // 或是你原本有的 plugins
     return {
       server: {
         port: 3000,
